bf514b68f83ce4b28fcfed2c56235bd5a9264fb9
ilikehome <ilikehome@163.com>
2025-05-31 23:33:37 +0800

    vite for storm

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'  // 新增：用于路径解析
+import path from 'path'
+import { fileURLToPath } from 'url'; // 新增：用于将 URL 转换为文件路径
+
+// 获取当前模块的文件路径
+const __filename = fileURLToPath(import.meta.url);
+// 获取当前模块所在的目录
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
